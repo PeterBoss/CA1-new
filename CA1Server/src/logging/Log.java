5059150c130857e2a0fcbaa5bcffa6e0a4753081
PeterBoss <cph-pt75@cphbusiness.dk>
2016-09-11 11:17:27 +0200

    re-enabled logging

---
+++ CA1Server/src/logging/Log.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logging;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ *
+ * @author josephawwal
+ */
+public class Log {
+
+    public static Properties initProps(String propertyFile) {
+
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream(propertyFile)) {
+
+            properties.load(is);
+
+        } catch (IOException ex) {
+
+            System.out.println(String.format("Could not locate the %$1$s file.", propertyFile));
+
+            return null;
+        }
+        return properties;
+    }
+
+    public static void setLogFile(String logFile, String className) {
+
+        try {
+
+            Logger logger = Logger.getLogger(className);
+            FileHandler fileTxt = new FileHandler(logFile);
+            java.util.logging.Formatter formatterTxt = new SimpleFormatter();
+            fileTxt.setFormatter(formatterTxt);
+            logger.addHandler(fileTxt);
+
+        } catch (IOException | SecurityException ex) {
+            Logger.getLogger(className).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeLogger(String logger) {
+
+        for (Handler h : Logger.getLogger(logger).getHandlers()) {
+
+            System.out.println("Closing log");
+            h.close();
+        }
+    }
+
+    public static Logger getLogger(String logFile, String className) {
+
+        Logger logger;
+
+        try {
+
+            logger = Logger.getLogger(className);
+            FileHandler fileTxt = new FileHandler(logFile);
+            java.util.logging.Formatter formatterTxt = new SimpleFormatter();
+            fileTxt.setFormatter(formatterTxt);
+            logger.addHandler(fileTxt);
+
+        } catch (IOException | SecurityException ex) {
+
+            Logger.getLogger(className).log(Level.SEVERE, null, ex);
+            return null;
+
+        }
+
+        return logger;
+
+    }
+
+}
